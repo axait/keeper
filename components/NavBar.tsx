e07d10a2989018acc371663565407062590223cb
Axait-th <axaitth@gmail.com>
2025-10-21 17:36:38 +0500

    initial commmit and Coded navbar succesfully.

---
+++ components/NavBar.tsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import Image from 'next/image';
+import "@/styles/navbar.css";
+
+const NavBar = () => {
+    return (
+        <div>
+            <nav
+                // absolute
+                className='
+                h-[50px] w-[98%]
+                flex justify-between items-center
+                px-5
+                mx-[1%] mt-1 mb-2
+                rounded
+                bg-[#512fa754]
+                opacity-90
+                navbar-active-hover
+                '
+            >
+                <h1
+                    className='irish-grover-regular'
+                >Keeper</h1>
+                <ul
+                    className='
+                flex gap-1
+                '
+                >
+                    <li>
+                        <Image src="/github-brands-solid-full.svg" alt="avatar" width={30} height={30} />
+                    </li>
+                    <li>
+                        <a
+                            href="https://github.com/axait/"
+                            className='irish-grover-regular text-sm'
+                        >@axait</a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    )
+}
+
+export default NavBar

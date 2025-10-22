3537849273f1332a5716e892e2e50b503e130083
Axait-th <axaitth@gmail.com>
2025-10-22 17:10:31 +0500

    create separate folder for types.

--- app/api/todos/get/route.ts
+++ app/api/todos/get/route.ts
@@ -1,6 +1,14 @@
-import { NextResponse } from 'next/server'
- 
-export async function GET() {
-  return NextResponse.json({ message: 'Still Under Development' })
-}
+import { NextResponse } from "next/server";
+import { connectToDB } from "@/lib/mongodb";
+import { Todo } from "@/models/Todo";
+import { TodosType } from "@/types/TodosType.ts";
 
+export async function GET() {
+  try {
+    await connectToDB();
+    const todos: TodosType[] = await Todo.find();
+    return NextResponse.json({ success: true, todos });
+  } catch (error: any) {
+    return NextResponse.json({ success: false, error: error.message });
+  }
+}

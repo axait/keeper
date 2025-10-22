039251ad9a6635a0db3b0ef92e56aeb82afcb293
Axait-th <axaitth@gmail.com>
2025-10-22 15:35:04 +0500

    add proper props of todo component and separate scss of Input Field.

--- components/Todo.tsx
+++ components/Todo.tsx
@@ -2,18 +2,72 @@
 
 import React from 'react'
 import "@/styles/Todo.scss";
+import "@/styles/InputField.scss";
 
-
-function Todo({title, description, date, time, status}:{
+export type TodosType = {
+    key: number|string,
     title: string,
     description: string,
     date: string,
     time: string,
-    status: "not-completed"|"completed",}) {
+    status: "InCompleted" | "Completed"
+}
+
+
+function Todo({ key, title, description, date, time, status }:TodosType) {
     return (
         <>
-        {/* COMPLETE TODO FOR DETAIL KEEPER */}
+            {/* COMPLETE TODO FOR DETAIL KEEPER */}
             <li
+                key={key}
+                className='
+                bg-[#1a1a1a]
+                min-h-[50px]
+                my-1.5 mx-1
+                rounded 
+                flex items-center
+                '
+            >
+                {/* INPUT FIELD */}
+                <label
+                    className="
+                form-control
+                ml-2 md:ml-3
+                ">
+                    <input type="checkbox" name="checkbox" />
+                </label>
+
+                {/* STATUS */}
+                <div
+                    className='
+                    ml-2 md:ml-3
+                    '
+                >
+                    <span
+                        className='
+                        font-sans
+                        text-sm md:text-base
+                        '
+                    >{status}</span>
+                </div>
+
+                <span
+                    className='
+                    font-sans
+                    text-sm md:text-base
+                    ml-2
+                    '
+                >{title}</span>
+
+            </li>
+        </>
+    )
+}
+
+export default Todo
+
+
+{/* <li
                 className='
                 bg-[#1a1a1a]
                 h-[50px]
@@ -42,9 +96,4 @@ function Todo({title, description, date, time, status}:{
                     '
                 >Lorem ipsum dolor sit amet consectetur adipisicing </span>
 
-            </li>
-        </>
-    )
-}
-
-export default Todo
+            </li> */}
--- components/TodoList.tsx
+++ components/TodoList.tsx
@@ -1,30 +1,68 @@
 "use client";
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import FilterBar from './FilterBar';
 import Todo from './Todo';
+import { type TodosType } from './Todo';
 import "@/styles/todo_list.scss";
 
+
 const TodoList = () => {
-  return (
-    <div>
-        <ul
-            className='
+
+    const [todos, setTodos] = useState<TodosType[]>([]);
+    // FOR DUMMY TODOS DATA
+    useEffect(() => {
+        setTodos([
+            {
+                key: 1,
+                title: "Buy Milk",
+                description: "Buy milk from the AK store",
+                date: "2023-05-15",
+                time: "10:00 AM",
+                status: "Completed"
+            },
+            {
+                key: 2,
+                title: "Buy Bread",
+                description: "Buy bread from the local bakery",
+                date: "2023-05-16",
+                time: "11:00 AM",
+                status: "InCompleted"
+            },
+            {
+                key: 3,
+                title: "Buy Eggs",
+                description: "Buy eggs from the market",
+                date: "2023-05-17",
+                time: "12:00 PM",
+                status: "InCompleted"
+            }
+        ]);
+    }, [])
+
+
+    return (
+        <div>
+            <ul
+                className='
             flex flex-col
             w-[98%] md:w-[65%]
             mx-auto
             '
-        >
-            <FilterBar/>
-            <Todo/>
-            <Todo/>
-            <Todo/>
-            <Todo/>
-            
-        </ul>
-      
-    </div>
-  )
+            >
+                <FilterBar />
+                {
+                    todos.map(
+                        todo => (
+                            <Todo key={todo.key} title={todo.title} description={todo.description} date={todo.date} time={todo.time} status={todo.status} />
+                        )
+                    )
+                }
+
+            </ul>
+
+        </div>
+    )
 }
 
 export default TodoList

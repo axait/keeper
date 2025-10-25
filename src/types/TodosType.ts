96975a10710bb500f3a1f6e7a73a701bc2def1e0
Axait-th <axaitth@gmail.com>
2025-10-25 14:28:03 +0500

    shift the code in src folder

---
+++ src/types/TodosType.ts
@@ -0,0 +1,10 @@
+
+export type TodosType = {
+    key?: number | string,
+    _id?: number | string,
+    title: string,
+    description: string,
+    date: string,
+    time: string,
+    status: "InComplete" | "Completed"
+}

9d57f106c157d339e16f9800f25dc3b352e75a6f
Axait-th <axaitth@gmail.com>
2025-10-27 21:35:16 +0500

    complete mySignJwt and myVerifyJwt but testing is left

--- src/lib/jwt.ts
+++ src/lib/jwt.ts
@@ -1,25 +1,48 @@
 import { SignJWT, jwtVerify } from "jose";
+import { logMe } from "./log";
 
 const JWT_SECRET = new TextEncoder().encode(process.env.JWT_SECRET);
-const algo = "HS256";
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export async function signJwt(payload: any) {
-    const now = Math.floor( Date.now() / 1000 )
-    
+export async function mySignJwt(payload: any): Promise<string> {
+    const now = Math.floor(Date.now() / 1000)
+
     console.log("sign token utility")
 
-    const token = await new SignJWT()
-                        .setExpirationTime("1m")
-                        .setSubject(payload)
-                        .setIssuedAt(now)
+    const token = await new SignJWT({
+        ...payload,
+        iat: now,
+        iss: "keeper-wheat.vercel.com",
+        aud: "keeper-frontend",
+    })
+        .setProtectedHeader({ alg: "HS256" })
+        .setIssuedAt()
+        .setExpirationTime("1m") // expires in 1 minute
+        .sign(JWT_SECRET)
+
 
     return token;
 
 }
 
 
-export async function verifyJwt(token: string) {
+export async function myVerifyJwt(token: string): Promise<{ valid: boolean, data: null | object }> {
     console.log("verify token utility")
+
+    try {
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        const payload: any = await jwtVerify(token, JWT_SECRET, {
+            issuer: "keeper-wheat.vercel.com",
+            audience: "keeper-frontend",
+        })
+        logMe(payload)
+
+        return { valid: true, data: payload };
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    } catch (error: any) {
+        logMe(error.message)
+
+        return { valid: false, data: null };
+    }
 }
 

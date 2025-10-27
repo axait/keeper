10f7bb5fd4dfee6e26e7911aad46b84254566641
Axait-th <axaitth@gmail.com>
2025-10-27 20:03:51 +0500

    add page.tsx extension

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'page.tsx', 'md', 'mdx'],
+  
 };
 
 export default nextConfig;

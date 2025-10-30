522bdb78f81ddd0d89b2625b88176f7e6816212b
Axait-th <axaitth@gmail.com>
2025-10-30 17:57:40 +0500

    complete the middleware code for now.

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,13 +1,46 @@
 import { NextResponse, NextRequest } from 'next/server'
+import { myVerifyJwt } from './lib/jwt';
 
-export function middleware(request: NextRequest) {
 
-  // ⁡⁢⁢⁢​‌‍‌COMPLETE ME MY VALIDATING TOKEN HERE FOR THE USER AND THEN ALLOW REQUEST TO THE NEXT ROUTE​⁡
+const exceptionalRoutes = ['/api', '/todo'];
 
-  console.log("🚀 Middleware triggered!");
-  return NextResponse.redirect(new URL('/', request.url))
+export async function middleware(req: NextRequest) {
+  console.log(`🚀 Middleware triggered! ${req.nextUrl.pathname}`);
+
+  // checking for exceptional routes
+  if (exceptionalRoutes.includes(req.nextUrl.pathname)) {
+    console.log("[OK] passed")
+    return NextResponse.next();
+  }
+
+  // trying to extract token
+  const token = req.cookies?.get('token')?.value;
+
+  // checking whther token exist or not.
+  if (!token) {
+    console.log("no token")
+    return NextResponse.redirect(new URL('/404', req.url))
+  }
+
+  // checking whther token exist or not.
+  const { valid, payload } = await myVerifyJwt(token)
+
+  if (!valid) {
+    console.log("InValid token")
+    return NextResponse.redirect(new URL('/404', req.url))
+  }
+
+  // adding headers with data for backend
+  const res = NextResponse.next();
+  res.headers.set("x-user-id", payload?.userId || "");
+  res.headers.set("x-session-id", payload?.sessionId || "");
+  // res.headers.set("x-user-level", payload?.level || "");
+
+
+  return res;
 }
 
 export const config = {
   matcher: ['/api/:path*', '/todo/:path*'],
 }
+
